6698f2e9defe07491d1266cf73d37170623e31e9
AndreaPicasso <andrea0pica@gmail.com>
2016-05-16 21:43:43 +0200

    f

--- chatapp.php
+++ chatapp.php
@@ -1,25 +1,24 @@
-<html>
-  <head>
-    <title>Account</title>
-      <link rel="stylesheet" href="css/all.css" type="text/css">
-      <link rel="stylesheet" href="css/chatapp.css" type="text/css">
-      <link rel="stylesheet" href="css/home.css" type="text/css">
-    <link rel="stylesheet" href="css/account.css" type="text/css">
-  </head>
-  <body>
+<?php   
+
+ require("php/parameters.php");?>
+
    <div id="main">
-        
-      
-        <div id="container">
-            <span id="chat">
-                <div id="chatmessagesend">
-                    Messaggio inviato
-                </div>
-                <div id="chatmessagerecieved">
-                    Messaggio ricevuto
-                </div>
+        <?php require("parts/banner_account.php"); ?>
+        <div id="container_home">
+            <span id="chat_container">
+                 <span id="chat_content">
+                     <!--
+                    <div class="chatmessagesend">
+                        Messaggio inviato
+                    </div>
+                    <div class="chatmessagerecieved">
+                        Messaggio ricevuto
+                    </div>
+                     -->
+                </span>
                 <div id="writemessage">
-                    <form  action="" method="post" >
+                    <form  action="#" method="post" >
+                        Destinatario: <input readonly="text" id="destinatario" name="destinatario" value="">
                         <input type="text" id="message" name="message" placeholder="Scrivi qui il tuo messaggio">
                        
                         <br>   
@@ -28,22 +27,108 @@
                 </div>
             </span>
             <span id="chatlist">
-                <div id="listelement1">
-                    Mario Rossi
-                </div>
-                 <div id="listelement2">
-                    Alessio Merlo
-                </div>
+                <?php addChat(); ?>
             </span>
     
         </div>
     
          
     </div>
-    
-    
+
+    <script type="text/javascript">
+        /* CHIAMATA AJAX */
+        function riempiChat(other) {
+        var dest = document.getElementById("destinatario");
+            dest.value =other;
+        var chat = document.getElementById("chat_content");
+        chat.innerHTML = synchcall(other);  
+    }
+
+        function synchcall(other) {  
+            
+        xhr = getXMLHttpRequestObject();
+
+        xhr.open("POST", "php/riempiChat.php/?other="+other, false);
+
+            xhr.send(); 
+ 
+
+        if (xhr.readyState==4 && xhr.status==200) { 
+              out = xhr.responseText; 
+              return out;
+            }
+        else 
+            return "Error<br/><strong>Code:</strong> " + xhr.status + "<br/><strong>Reason:<strong> " + xhr.statusText;
+    }
         
-    
-    
+        
+    </script>
   </body>
+
+
+<?php
+    //set up page
+    function addChat(){
+        $con = mysqli_connect(SERVER,USER,PSW);
+        mysqli_select_db($con,DB);
+        $email = $_SESSION['email'];
+        $query_mittente = 'SELECT DISTINCT destinatario
+                FROM message
+                WHERE mittente="'.$email.'" ;';
+         $query_destinatario = 'SELECT DISTINCT mittente
+                FROM message
+                WHERE destinatario="'.$email.'"
+                AND mittente NOT IN
+                    (SELECT DISTINCT destinatario
+                    FROM message
+                    WHERE mittente="'.$email.'");';
+        $res = mysqli_query($con,$query_mittente);
+        $numrows= mysqli_num_rows($res);
+        for($i = 0; $i<$numrows; $i++){
+            $row = mysqli_fetch_assoc($res);
+             //Riempi la chat con il primo
+            /*
+            if($i==0)
+                $toShow = $row['destinatario'];
+                */
+            $mod = $i%2;
+            echo '<div class="listelement'.$mod.'" onclick="riempiChat(\''.$row['destinatario'].'\')">'.$row['destinatario'].'</div>';
+        }
+        $res = mysqli_query($con,$query_destinatario);
+        $numrows= mysqli_num_rows($res);
+        for($i = 0; $i<$numrows; $i++){
+            $row = mysqli_fetch_assoc($res);
+            $mod = $i%2;
+            echo '<div class="listelement'.$mod.'" onclick="riempiChat(\''.$row['mittente'].'\')">'.$row['mittente'].'</div>';
+        }
+    }
+
+?>
+
+<?php
+ //send message
+if(isset($_POST['sendbutton'])){
+    $text = $_POST['message'];
+    $dest = $_POST['destinatario'];
+    $email = $_SESSION['email'];
+    $ok=true;
+    if(strcmp($dest,"")==0){
+        echo '<script type="text/javascript">window.alert("Nessun destinatario selezionato")</script>';
+        $ok = false;
+    }
+    if(strcmp($text,"")==0){
+        echo '<script type="text/javascript">window.alert("Impossibile inviare un messaggio vuoto")</script>';
+        $ok=false;
+    }
+    if($ok){
+         $con = mysqli_connect(SERVER,USER,PSW);
+        mysqli_select_db($con,DB);
+        $text = mysqli_real_escape_string($con,$text);
+        $query = "INSERT INTO message (mittente, destinatario, testo, datames) VALUES ('".$email."','".$dest."','".$text."',FROM_UNIXTIME(".time()."));";
+        $res = mysqli_query($con,$query);
+        echo '<script type="text/javascript">riempiChat(\''.$dest.'\')</script>';
+    }
+}
+
+?>
 </html>
